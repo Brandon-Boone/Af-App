ee438841b368258f9ed0dcc4cd8ae1d6b7d6e86d
Sern_Intern <Sern_Intern@SERN-INTERN38.SERN.MIL>
2022-07-05 12:32:35 -0400

    message

---
+++ repos/Notes/Notes/Notes/Views/Test.xaml.g.cs
@@ -0,0 +1,17 @@
+﻿[assembly: global::Xamarin.Forms.Xaml.XamlResourceIdAttribute("Notes.Views.TestPage.xaml", "Views/TestPage.xaml", typeof(global::Notes.Views.TestPage))]
+
+namespace Notes.Views
+{
+
+
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("Views\\TestPage.xaml")]
+    public partial class TestPage : global::Xamarin.Forms.ContentPage
+    {
+
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private void InitializeComponent()
+        {
+            global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(TestPage));
+        }
+    }
+}
